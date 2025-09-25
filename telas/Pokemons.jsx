adddef7032375ad7aac4180a897ed81c7601a02c
MauricioTSbichels <mauricio_bichels@estudante.sesisenai.org.br>
2025-09-25 12:26:47 -0300

    create

--- telas/Pokemons.jsx
+++ telas/Pokemons.jsx
@@ -3,6 +3,9 @@ import { ImageBackground, StyleSheet, StatusBar, Text, View } from "react-native
 export default function Pokemons(){
     return (
         <View >
+            <Image
+               source={require('../imagens/logomarca_pokemon.png')}            
+            />
             <Text>Teste</Text>
             <StatusBar hidden={true}/>
         </View>
