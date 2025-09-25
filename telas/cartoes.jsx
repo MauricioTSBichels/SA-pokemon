2f4c3a46af7d235fa524c2552b20cd58729e94e9
MauricioTSbichels <mauricio_bichels@estudante.sesisenai.org.br>
2025-09-25 12:39:35 -0300

    alt telas

--- telas/cartoes.jsx
+++ telas/cartoes.jsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-export default function cartoes() {
+export default function cartoes({imagem, titulo, onPress}) {
   return (
-    <View>
-      <Text>cartoes</Text>
-    </View>
+    <TouchableOpacity>
+      <Image
+       source={imagem}>
+      </Image>
+      <Text>{titulo}</Text>
+    </TouchableOpacity>
   )
 }
 
-const styles = StyleSheet.create({})
+export const estilos = StyleSheet.create({})
