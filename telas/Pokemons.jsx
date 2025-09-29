e7fcfa36b6e7c60145bd102c324e73fc03a84293
MauricioTSbichels <mauricio_bichels@estudante.sesisenai.org.br>
2025-09-29 07:52:15 -0300

    cartoes

--- telas/Pokemons.jsx
+++ telas/Pokemons.jsx
@@ -1,4 +1,5 @@
 import { ImageBackground, StyleSheet, StatusBar, Text, View } from "react-native";
+import cartoes from "./cartoes";
 
 export default function Pokemons(){
     return (
@@ -7,8 +8,14 @@ export default function Pokemons(){
                source={require('../imagens/logomarca_pokemon.png')}   
                style={{margin: 'auto', marginTop: 30}}         
             />
-            <Text>Teste</Text>
             <StatusBar hidden={true}/>
+            <View>
+                <Cartoes
+                  imagem={require('../imagens/Bulbassauro.png')}
+                  titulo={"Bulbassauro"}
+                  onPress={()=>navigation.navigative("Bulbassauro")}
+                />
+            </View>
         </View>
     );
 }
